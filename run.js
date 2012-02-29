27e8db9e8917b0eab638b19aee615ccd8b749d0c
Kun Wang <ifreedom.cn@gmail.com>
2012-03-01 00:46:50 +0800

    Update run.js.

--- run.js
+++ run.js
@@ -24,10 +24,14 @@ app.init = function(){
                    if (key == nge2.PSP_BUTTON_UP)
                      quit = true;
                  }, 0);
+  nge2.SetScreenColor(255,255,255,255);
+
   return 0;
 }
 
-var polygons_x2 = [ 200.0, 300.0, 400.0 ]
+var polygons_x1 = [ 200.0, 300.0, 400.0, 300.0 ]
+  , polygons_y1 = [ 10.0, 10.0, 70.0, 70.0 ]
+  , polygons_x2 = [ 200.0, 300.0, 400.0 ]
   , polygons_y2 = [ 80.0, 80.0, 160.0 ]
   , polygons_x3 = [ 400.0, 450.0, 350.0 ]
   , polygons_y3 = [ 160.0, 250.0, 250.0 ]
@@ -36,11 +40,17 @@ var polygons_x2 = [ 200.0, 300.0, 400.0 ]
 app.mainloop = function(){
   try {
     nge2.InputProc();
+    // nge2.ShowFps(); // can not print
 
     nge2.BeginScene(1);
     nge2.DrawLine(0, 0, 480, 272, red, dtype);
     nge2.DrawRect(20,20,200,200,red,dtype);
     nge2.FillRect(40,40,100,100,red,dtype);
+	nge2.DrawCircle(100,100,20,blue,dtype);
+	nge2.FillCircle(150,150,20,yellow,dtype);
+	nge2.DrawEllipse(200,200,20,40,blue,dtype);
+	nge2.FillEllipse(250,250,40,20,yellow,dtype);
+	nge2.DrawPolygon(polygons_x1,polygons_y1,4,blue,dtype);
     nge2.FillPolygon(polygons_x2,polygons_y2,3,red,dtype);
     nge2.FillPolygonGrad(polygons_x3,polygons_y3,3,polygons_colors, dtype);
     nge2.EndScene();
