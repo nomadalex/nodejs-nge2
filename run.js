710b9eaf731c4777232bc216f2995b947a2566a9
Kun Wang <ifreedom.cn@gmail.com>
2012-02-29 16:19:49 +0800

    Update run.js.

--- run.js
+++ run.js
@@ -6,8 +6,12 @@ var app = new nge2.NgeApp();
 
 var quit;
 
+var red = nge2.CreateColor(255, 0, 0, 255, nge2.DISPLAY_PIXEL_FORMAT_8888);
+
+console.log("red color : " + red);
+
 app.init = function(){
-  console.log(nge2.NgeApp.getCurrent());
+  console.log("current app : " + nge2.NgeApp.getCurrent());
   nge2.Init(nge2.INIT_VIDEO);
   nge2.InitInput(function(key) {
                    console.log(key);
@@ -21,7 +25,16 @@ app.init = function(){
 }
 
 app.mainloop = function(){
-  nge2.InputProc();
+  try {
+    nge2.InputProc();
+
+    nge2.BeginScene(1);
+    nge2.DrawLine(0, 0, 480, 272, red, nge2.DISPLAY_PIXEL_FORMAT_8888);
+    nge2.EndScene();
+  }
+  catch (err) {
+    console.log("err : " + err);
+  }
 
   if (quit)
     return nge2.NGE_APP_QUIT;
