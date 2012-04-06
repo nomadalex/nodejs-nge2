32317a3f12b4a9c93fe28967bd6eceb2a1d589ad
Kun Wang <ifreedom.cn@gmail.com>
2012-04-06 22:26:38 +0800

    Update Nodejs.cmake.
    Fix header path on linux.
    Simulate node-waf behaviour, copy target library to per-configure directory e.g. Debug and Release.

--- CMake/Nodejs.cmake
+++ CMake/Nodejs.cmake
@@ -18,7 +18,7 @@ macro(prepare_nodejs)
   set(NODE_ROOT "" CACHE PATH "Path to nodejs root dir")
   if(NOT NODE_ROOT)
 	if($ENV{NODE_ROOT})
-	  set(NODE_ROOT $ENV{NODE_ROOT} FORCE)
+	  set(NODE_ROOT $ENV{NODE_ROOT} CACHE PATH "Path to nodejs root dir" FORCE)
 	else()
 	  find_program(node_exe_path node)
 	  mark_as_advanced(node_exe_path)
@@ -28,9 +28,13 @@ macro(prepare_nodejs)
 		  get_filename_component(node_dir ${node_dir}/.. ABSOLUTE)
 		endif()
 		set(NODE_ROOT ${node_dir} CACHE PATH "Path to nodejs root dir" FORCE)
+	  else()
+		message(FATAL_ERROR "Can not find nodejs!")
 	  endif()
 	endif()
   endif()
+
+  message(STATUS "Find node root : ${NODE_ROOT}")
 
   if(WIN32)
 	if(BIN_VERSION)
@@ -42,7 +46,7 @@ macro(prepare_nodejs)
 	endif()
   else()
 	link_directories(${NODE_ROOT}/lib)
-	include_directories(${NODE_ROOT}/include)
+	include_directories(${NODE_ROOT}/include/node)
   endif()
 
   if(Linux)
@@ -54,6 +58,19 @@ endmacro()
 
 function(add_node_extension target)
   add_library(${target} SHARED ${ARGN})
-  target_link_libraries(${target} node)
-  set_target_properties(${target} PROPERTIES SUFFIX ".node")
+  if(WIN32)
+	set_target_properties(${target} PROPERTIES SUFFIX ".node")
+	target_link_libraries(${target} node)
+  endif()
+  if(NOT WIN32)
+	# simulate node-waf behaviour, copy target to per-configure directory e.g. Debug and Release
+	get_target_property(LIB_LOCATION ${target} LOCATION)
+	add_custom_command(
+  	  TARGET ${target}
+  	  POST_BUILD
+  	  COMMAND "${CMAKE_COMMAND}" -E copy
+  	  "${LIB_LOCATION}"
+  	  "${CMAKE_BINARY_DIR}/${CMAKE_BUILD_TYPE}/${target}.node"
+  	  )
+  endif()
 endfunction()
